c9ac2d69adf54edb5ebef4aeb527b3b1e5fa3693
Andruby <zhaomeng@daojia-inc.com>
2022-03-03 16:20:20 +0800

    修改路由规则

--- routes/route.php
+++ routes/route.php
@@ -26,9 +26,11 @@ Route::group([
     'middleware' => config('deep_docs.route.middleware')
 ], function (Router $router) {
 
-    $router->get('/{router_name?}', 'DocsController@index')
-        ->where(['router_name'=>'^(?!'.config('admin.route.prefix').'$)[\s\S]+$'])
+    $router->get('/', 'DocsController@index')
         ->name('docs.index');
+    $router->get('/p_{router_name?}', 'DocsController@index')
+        ->name('docs.project');
+
     $router->get('/{router_name}/{version}', 'DocsController@version')->name('docs.version');
     $router->get('/{router_name}/{version}/{doc_id}.html', 'DocsController@detail')->name('docs.detail');
 
