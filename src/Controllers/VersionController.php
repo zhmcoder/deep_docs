d67b5b2ee190e6fe531a7cab7ebce61818ccb39c
fanzewei <fanzewei@yongche.com>
2022-03-15 22:41:55 +0800

    页面优化

--- src/Controllers/VersionController.php
+++ src/Controllers/VersionController.php
@@ -3,9 +3,14 @@
 namespace Andruby\DeepDocs\Controllers;
 
 use Andruby\DeepAdmin\Controllers\ContentController;
-
+use Andruby\DeepAdmin\Grid;
 
 class VersionController extends ContentController
 {
+    protected function grid_list(Grid $grid): Grid
+    {
+        $grid->dialogForm($this->form()->isDialog()->className('p-15'));
 
+        return $grid;
+    }
 }
